3ac06fb21826321ca0ecd17d0d2cca5e6fd2323e
Sahir <shehzadss@vcu.edu>
2018-04-29 21:10:50 -0700

    Allow CORS

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(allowCrossDomain);
 app.use( function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
