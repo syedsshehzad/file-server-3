f60481621898fc18ec41aa2545c3b39fa5cefe53
Sahir <shehzadss@vcu.edu>
2018-04-29 20:07:49 -0700

    6th

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.set("view engine", "handlebars");
 
 
 app.get('/', (req, res) => {
-	res.sendFile('index.html', err => {if (err) throw err})
+	res.sendFilndex.html', err => {if (err) throw err})
 });
 
 app.post('/', (req, res) => {
