370b161e8c1e9b0fef71dfd2a3e4dcb278f9367b
Sahir <shehzadss@vcu.edu>
2018-04-29 21:07:00 -0700

    enabled CORS

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(allowCrossDomain);
+app.use( function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
+
 var mongoose = require('mongoose');
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/fileserveDB";
 mongoose.Promise = Promise;
